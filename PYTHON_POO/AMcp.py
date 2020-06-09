f3f62fea667e447318f62293b281695f81a3a780
Davi Honorato <davii.silva64@gmail.com>
2020-06-09 15:34:20 -0300

    Conta Poupança

--- PYTHON_POO/AMcp.py
+++ PYTHON_POO/AMcp.py
@@ -1,1 +1,10 @@
 # Aqui será colocada o algorítmo da conta poupança.
+from PYTHON_POO.AMconta import Conta
+
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        if self._saldo < valor:
+            print('Saldo insuficiente.')
+
+        self._saldo -= valor
