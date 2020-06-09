036d55967bac6bbe3a81aa5317bcd9b5014aef03
Davi Honorato <davii.silva64@gmail.com>
2020-06-09 15:53:36 -0300

    Conta Corrente

--- PYTHON_POO/AMcc.py
+++ PYTHON_POO/AMcc.py
@@ -1,1 +1,20 @@
 # Aqui será colocada o algorítmo da conta corrente.
+from PYTHON_POO.AMconta import Conta
+
+
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo, limite):
+        super().__init__(agencia, conta, saldo)
+        self._limite = limite
+
+    @property
+    def limite(self):
+        return self._limite
+
+    def sacar(self, valor):
+        if (self._saldo + self._limite) < valor:
+            print('Saldo insuficiente.')
+            return
+
+        self._saldo -= valor
+        self.detalhes()
