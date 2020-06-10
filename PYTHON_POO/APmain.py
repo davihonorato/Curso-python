96dd2cbdc9548dd5863b0825ebb52df5884ab925
Davi Honorato <davii.silva64@gmail.com>
2020-06-10 19:55:19 -0300

    (AP) Testando outros métodos; final

--- PYTHON_POO/APmain.py
+++ PYTHON_POO/APmain.py
@@ -3,8 +3,32 @@ class Retangulo:
         self.x = x
         self.y = y
 
+    def __repr__(self):
+        return f"<class 'Retangulo({self.x}, {self.y})'>"
+
+    def area(self):
+        return self.x * self.y
+
     def __eq__(self, other):  # Possibilita que os objetos sejam testados (com "==") para saber se são iguais
         if self.x == other.x and self.y == other.y:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        a1 = self.area()
+        a2 = other.area()
+
+        if a1 > a2:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        a1 = self.area()
+        a2 = other.area()
+
+        if a1 < a2:
             return True
         else:
             return False
@@ -12,4 +36,8 @@ class Retangulo:
 
 r1 = Retangulo(10, 20)
 r2 = Retangulo(10, 20)
+print(r1)  # Testando o método __repr__
+print(r2)  # Testando o método __repr__
 print(r1 == r2)  # Testando o método __eq__
+print(r1 > r2)  # Testando o método __gt__
+print(r1 < r2)  # Testando o método __lt__
