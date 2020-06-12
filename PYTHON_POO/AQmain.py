4de8e01ad1bc42778b1890b746510577f7edd69c
Davi Honorato <davii.silva64@gmail.com>
2020-06-11 21:10:18 -0300

    (AQ) Adicionando métodos mágicos

--- PYTHON_POO/AQmain.py
+++ PYTHON_POO/AQmain.py
@@ -1,3 +1,17 @@
 '''
 A Guide to Python's Magic Methods: https://rszalski.github.io/magicmethods/
 '''
+
+
+class A:
+    def __new__(cls, *args, **kwargs):  # É executado antes do init. argumentos: *args, argumentos nomeados: **kwargs.
+        return super().__new__(cls)  # permite que o init seja executado (pesquisar sobre)
+
+    def __init__(self):  # "Inicializador" da classe
+        print('Eu sou o INIT')
+
+    def __del__(self):  # Finaliza o programa do python. Às vezes pode não executar.
+        print('Conteúdo coletado.')
+
+
+a = A()
