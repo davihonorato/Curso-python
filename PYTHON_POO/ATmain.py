502b3fa7a5052e5deb7928fad6ec25f333a33006
Davi Honorato <davii.silva64@gmail.com>
2020-06-23 14:08:01 -0300

    classes não sobrescrevem o atributo (AT)

--- PYTHON_POO/ATmain.py
+++ PYTHON_POO/ATmain.py
@@ -3,21 +3,24 @@ class Meta(type):
         if name == 'A':
             return type.__new__(mcs, name, bases, namespace)
 
-        if 'b_fala' not in namespace:
-            print(f'Oi, você precisa criar o método b_fala em {name}')
-        else:
-            if not callable(namespace['b_fala']):
-                print(f'b_fala precisa ser um método, não um atributo em {name}')
+        if 'attr_classe' in namespace:
+            print(f'{name} tentou sobrescrever o atributo.')
+            del namespace['attr_classe']
 
         return type.__new__(mcs, name, bases, namespace)
 
 
 class A(metaclass=Meta):
-    def fala(self):
-        self.b_fala()
+    attr_classe = 'Valor A'
 
 
 class B(A):
-    # b_fala não pode ser um atributo de classe e sim um método, caso contrário, irá dar "Erro"
-    def b_fala(self):
-        print('Classe de B')
+    attr_classe = 'Valor B'
+
+
+class C(B):
+    attr_classe = 'Valor C'
+
+
+c = C()
+print(c.attr_classe)
