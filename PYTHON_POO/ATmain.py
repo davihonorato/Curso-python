f261d62a8130bc7c6322f77faebb4bf6e605e12a
Davi Honorato <davii.silva64@gmail.com>
2020-06-23 13:47:54 -0300

    Metaclasses (AT)

--- PYTHON_POO/ATmain.py
+++ PYTHON_POO/ATmain.py
@@ -1,16 +1,23 @@
-'''class Meta(type):
-    pass
-'''
+class Meta(type):
+    def __new__(mcs, name, bases, namespace):
+        if name == 'A':
+            return type.__new__(mcs, name, bases, namespace)
 
-class A:
+        if 'b_fala' not in namespace:
+            print(f'Oi, você precisa criar o método b_fala em {name}')
+        else:
+            if not callable(namespace['b_fala']):
+                print(f'b_fala precisa ser um método, não um atributo em {name}')
+
+        return type.__new__(mcs, name, bases, namespace)
+
+
+class A(metaclass=Meta):
     def fala(self):
         self.b_fala()
 
 
 class B(A):
+    # b_fala não pode ser um atributo de classe e sim um método, caso contrário, irá dar "Erro"
     def b_fala(self):
         print('Classe de B')
-
-
-b1 = B()
-b1.fala()
