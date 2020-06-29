6056b9de031096e45fffefcf026aa5ca6d9fd7a4
Davi Honorato <davii.silva64@gmail.com>
2020-06-29 19:27:50 -0300

    Docstring de funções 1 (AU)

--- PYTHON_POO/AUmain.py
+++ PYTHON_POO/AUmain.py
@@ -1,5 +1,7 @@
 from PYTHON_POO import AUumalinha
 from PYTHON_POO import AUvariaslinhas
+from PYTHON_POO import AUfuncao
 
 # help(AUumalinha)
-help(AUvariaslinhas)
+# help(AUvariaslinhas)
+help(AUfuncao)
