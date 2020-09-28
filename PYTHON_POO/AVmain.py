47e6b4b2f5b37ef520b8b31d37dba0b5d259a0b0
Davi Honorato <davii.silva64@gmail.com>
2020-09-28 00:55:00 -0300

    AVmain (desafio 01)

--- PYTHON_POO/AVmain.py
+++ PYTHON_POO/AVmain.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+
+
 class Pessoa:
     def __init__(self, nome, idade):
         self._nome = nome
@@ -13,24 +16,60 @@ class Pessoa:
 
 
 class Cliente(Pessoa):
-    def __init__(self, nome, idade, conta=None):
-        Pessoa.__init__(self, nome, idade)
-        pass
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
+        self.conta = None
+
+    def inserir_conta(self, conta):
+        self.conta = conta
 
 
 class Conta:
-    def __init__(self, agencia, numConta, saldo):
+    def __init__(self, agencia, conta, saldo):
         self.agencia = agencia
-        self.numConta = numConta
+        self.conta = conta
         self.saldo = saldo
 
-    def depositar(self, quantia):
-        pass
+    def depositar(self, valor):
+        self.saldo += valor
+        self.detalhes()
+
+    def detalhes(self):
+        print(f'Agência: {self.agencia} '
+              f'Conta: {self.conta} '
+              f'Saldo: {self.saldo} ')
+
+    @abstractmethod
+    def sacar(self, valor): pass
 
 
-class ContaCorrente:
-    pass
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        if self.saldo < valor:
+            print('Saldo insuficiente.')
+            return
+
+        self.saldo -= valor
+        self.detalhes()
 
 
-class ContaPoupanca:
-    pass
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo, limite=100):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+
+    def sacar(self, valor):
+        if self.saldo < valor:
+            print('Saldo insuficiente.')
+            return
+
+        self.saldo -= valor
+        self.detalhes()
+
+
+class Banco:
+    def __init__(self):
+        self.agencias = [1111, 2222, 3333]
+        self.clientes = []
+        self.contas= []
+
